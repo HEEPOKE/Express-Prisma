618c8acb3aad2e117823fd1a6852edad2ccb0fd9
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-21 11:50:01 +0700

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import config from "./config/config";
 import router from "./routes";
 
 const app = express();
+
+app.use(cors());
 
 app.use("/api", router);
 
