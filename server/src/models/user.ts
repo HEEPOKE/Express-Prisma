1eefc4a013beb7c006b50505abb4a78fdb804bc1
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-21 11:29:07 +0700

    Update user.ts

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,7 +1,9 @@
+import { Role } from "../enums/role";
+
 export interface UserModel {
   email: string;
   password: string;
   firstName: string;
   lastName: string;
-  role: string;
+  role: Role;
 }
