0d54b161460e7b4f5feb220cd7018b22052fbc6e
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-23 14:41:36 +0700

    :money_with_wings: Add helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import cors from "cors";
-import * as bodyParser from 'body-parser';
+import helmet from "helmet";
+import * as bodyParser from "body-parser";
 import config from "./config/config";
 import router from "./routes";
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
