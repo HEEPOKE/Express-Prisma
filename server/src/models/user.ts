b51c65ec315d161d6f14eefd0b63154e404f9dc5
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-23 09:09:04 +0700

    :money_with_wings: Add body-parser

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -7,16 +7,3 @@ export interface UserModel {
   lastName: string;
   role: Role;
 }
-
-// class User {
-//   id: number;
-//   name: string;
-//   email: string;
-//   password: string;
-
-//   constructor(id: number, name: string, email: string, password: string) {
-//     this.id = id;
-//     this.name = name;
-//     this.email = email;
-//     this.password = password;
-//   }
