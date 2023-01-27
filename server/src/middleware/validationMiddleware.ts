e30fbfbd0061faf020f8a8c391f21250604f549c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-27 15:39:49 +0700

    Update validationMiddleware.ts

--- server/src/middleware/validationMiddleware.ts
+++ server/src/middleware/validationMiddleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import config from "../config/config";
 
