37f8cf3259090ee685c97e5e998ccb86e90a282c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-01 14:42:27 +0700

    Update authController.test.ts

--- src/test/authController.test.ts
+++ src/test/authController.test.ts
@@ -1,4 +1,4 @@
-import supertest from "supertest";
+import request from "supertest";
 import server from "../server";
 import db from "../config/db";
 
@@ -18,9 +18,9 @@ describe("auth", () => {
     await db.user.deleteMany();
   });
 
-  describe("Register", () => {
+  describe("POST /api/auth/register", () => {
     it("should return a success message on register", async () => {
-      const response = await supertest(app).post("api/auth/register").send({
+      const response = await request(app).post("api/auth/register").send({
         firstName: "Test",
         lastName: "User",
         email: "Damon1FX@gmail.com",
@@ -34,14 +34,14 @@ describe("auth", () => {
     });
   });
 
-  describe("Login", () => {
+  describe("POST /api/auth/login", () => {
     it("should return a success message on login", async () => {
-      const response = await supertest(app)
+      const response = await request(app)
         .post("/api/auth/login")
-        .send({ email: "Damon1FX@gmail.com", password: "yoyo5555" });
+        .send({ email: "test@email.com", password: "password" });
 
       expect(response.status).toBe(200);
-      expect(response.body).toEqual({
+      expect(response.body).toBe({
         message: "Success",
       });
     });
